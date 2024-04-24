aae04ba4fe371bb277e4a62ef9c0e4cec07929ed
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-24 21:41:40 +0300

    Dal,service,repo,Manager Sınıfları Tanımlandı

---
+++ DataAccessLayer/Concrete/Repositories/GeneriRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Contexts;
+using EntityLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GeneriRepository<T> : IGenericDal<T> where T : class, new()
+    {
+        AgricultureContext context= new AgricultureContext();
+        public void Delete(T t)
+        {
+           context.Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            context.Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            context.Update(t);
+            context.SaveChanges();
+        }
+    }
+}
